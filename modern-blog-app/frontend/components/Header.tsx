7ec066e8dad11635316b78c506dc690fb6d8593a
SRK-RAJU <suddala.rajukumar123@gmail.com>
2026-05-16 15:27:34 +0530

    updated

--- modern-blog-app/frontend/components/Header.tsx
+++ modern-blog-app/frontend/components/Header.tsx
@@ -97,7 +97,7 @@ export default function Header() {
             </div>
             <div className="hidden sm:block">
               <p className="text-lg font-bold gradient-text">RJ</p>
-              <p className="text-xs text-slate-800 dark:text-slate-200 -mt-1">Modern Tech Blog</p>
+              <p className="text-xs text-slate-800 dark:text-slate-200 -mt-1">Modern Personal Tech Blog</p>
             </div>
           </Link>
         </motion.div>
